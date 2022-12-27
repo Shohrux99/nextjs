1ddc82f49cb5cc7784f36abf1274da15673c8767
Shohrux99 <b.shohrux@mail.ru>
2022-12-27 23:35:08 +0500

    ii

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import { Main } from 'components/UI/Main/Main'
+import SEO from 'components/SEO'
+import { fetchMultipleUrls } from 'services/fetchMultipleUrls'
+
+export default function Home({ data }) {
+  console.log('prr=>', process.env.NEXT_PUBLIC_TEST)
+  return (
+    <>
+      <SEO />
+      <Main />
+    </>
+  )
+}
+
+export async function getServerSideProps(context) {
+  const urls = ['/posts']
+  const data = await fetchMultipleUrls(urls)
+
+  return {
+    props: {
+      data
+    }
+  }
+}
